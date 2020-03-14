c0e1a69a07bf3cb43e2ec0f65e1d85345c63973d
LIHHAO <lih627@outlook.com>
2020-03-14 13:39:56 +0800

    add solutions 二分法+DP

--- LeetCodeSolutions/LeetCode_0300.py
+++ LeetCodeSolutions/LeetCode_0300.py
@@ -9,3 +9,25 @@ class Solution:
                 if nums[j] > _:
                     dp[j] = max(dp[j], dp[idx] + 1)
         return max(dp)
+
+    def lengthOfLIS2(self, nums: List[int]) -> int:
+        '''
+        binary search solution
+        '''
+        if not nums:
+            return 0
+        cell = []
+
+        for num in nums:
+            if not cell or num > cell[-1]:
+                cell.append(num)
+                continue
+            i, j = 0, len(cell)
+            while i < j:
+                mid = (i + j) // 2
+                if cell[mid] < num:
+                    i = mid + 1
+                else:
+                    j = mid
+            cell[i] = num
+        return len(cell)
