10341eca6456b64d8f1a050d3e8d688ff6336550
LIHHAO <lih627@outlook.com>
2020-04-14 00:14:48 +0800

    update Euclidean algorithm

--- maths/Euclidean_algorithm.py
+++ maths/Euclidean_algorithm.py
@@ -5,15 +5,22 @@ gcd(a, b) = gcd(a, a mod b)
 """
 
 
-def gcd(a, b):
-    if b == 0:
-        return a
-    if a % b:
-        return gcd(b, a % b)
-    return b
+# def gcd(a, b):
+#     if b == 0:
+#         return a
+#     if a % b:
+#         return gcd(b, a % b)
+#     return b
+
+def gcd(x, y):
+    if y == 0:
+        return x
+    else:
+        return gcd(y, x % y)
 
 
 if __name__ == '__main__':
     print(gcd(2, 5))
     print(gcd(10, 0))
     print(gcd(2, 4))
+
